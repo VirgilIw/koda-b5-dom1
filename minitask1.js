d0d9692fd2eb912c1bc2ab8346f7c9670a6ba8a6
Virgil I Ambar <imanuelambar11@gmail.com>
2025-11-25 14:02:33 +0700

    feat[celcius]: add feature to calculate celcius to fahrenheit

---
+++ minitask1.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+const fahrenheit = document.querySelector(".fahrenheit");
+const reamur = document.querySelector(".reamur");
+const kelvin = document.querySelector(".kelvin");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //
+  let answer = e.target.celcius.value;
+  //
+  window.setTimeout(() => {
+    celToFahren(answer);
+  }, 1000);
+});
+
+const celToFahren = (c) => {
+  let parse1 = parseInt(c);
+  let fahren = (4 / 5) * parse1;
+  let parse2 = parseFloat(fahren);
+  fahrenheit.textContent = parse2 + "°" + "fahrenheit";
+};
